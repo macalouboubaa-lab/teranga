27d04ef76187e4c011f19d7f3b7f3f488858acd0
macalouboubaa-lab <macalouboubaa@gmail.com>
2026-07-22 11:43:52 +0000

    Add Supabase env config and client setup

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,12 +2,26 @@ import { createClient } from "@supabase/supabase-js";
 /*
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+---------------------------------------------------------------------------------------
+Clé : NEXT_PUBLIC_SUPABASE_URL
+Valeur : sb_publishable_sIYhg5wCPde7Gg-dHxAkpA_U3oug3O9
+Note (Optionnel) : Laissez vide ou mettez "URL Supabase"
+----------------------------------------------------------------------------------------
+Pour NEXT_PUBLIC_SUPABASE_ANON_KEY :
+Clé : NEXT_PUBLIC_SUPABASE_ANON_KEY
+Valeur : *************************
+Note (Optionnel) : Laissez vide ou mettez "Clé Anon Supabase"
+----------------------------------------------------------------------------------------
 */
 
 const supabaseUrl = "https://mcistfdbrlbbkzjvsreb.supabase.co"!;
+const supabaseAnonKey = "sb_publishable_sIYhg5wCPde7Gg-dHxAkpA_U3oug3O9"!;	
+
+/*   ANCIEN LIENS 
+
+const supabaseUrl = "https://mcistfdbrlbbkzjvsreb.supabase.co"!;
 const supabaseAnonKey = "sb_publishable_p3qAAaIIBNQpCxBgy-RGnA_J-sOMxPd"!;	
-
-
+*/
 //j'enleve ma verification pour le moment 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("supabaseKey is required. Vérifie ton fichier .env.local");
